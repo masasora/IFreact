e5d97c6670c27ea2d2a008ffa27bfb1fa3c297c2
Masa <repurikasora@gmail.com>
2022-11-26 12:42:37 +0900

    URL追加(11/26/12:42)

--- components/twitter.js
+++ components/twitter.js
@@ -6,7 +6,7 @@ export default function Twi() {
     return(
         <div className={styles.twitter}>
 
-            <a className={styles.min} href="https://twitter.com/intent/tweet?text=%23IF　IFとは自分がゲームに費やした時間を資格勉強に費やしていたら何が取れていたかわかるツールです！ https://***" target="_blank" rel="noopener noreferrer">
+            <a className={styles.min} href="https://twitter.com/intent/tweet?text=%23IF　IFとは自分がゲームに費やした時間を資格勉強に費やしていたら何が取れていたかわかるツールです！ https://iflicensegacha.vercel.app/" target="_blank" rel="noopener noreferrer">
                             <Image
                                         alt="青い鳥"
                 src={Twiwhite}
